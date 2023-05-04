7d6f609a86490e8d5a6e7c55f1495d36caf8bb69
Doris Lin <lmh7026@gmail.com>
2023-05-04 15:31:10 -0400

    Refactor for diff class names in validate_model

--- app/model_helpers.py
+++ app/model_helpers.py
@@ -4,7 +4,7 @@ def validate_model(cls, id):
     try:
         id = int(id)
     except:
-        abort(make_response({"message":f"planet {id} invalid"}, 400))
+        abort(make_response({"message":f"{cls.__name__} {id} invalid"}, 400))
     
     model = cls.query.get(id)
     if not model:
